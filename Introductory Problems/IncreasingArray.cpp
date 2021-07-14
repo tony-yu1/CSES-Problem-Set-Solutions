640df44eafbbad018baa2f973511e6c869b638d6
purple221 <63677210+purple221@users.noreply.github.com>
2021-07-14 01:32:35 -0400

    Update IncreasingArray.cpp

--- Introductory Problems/IncreasingArray.cpp
+++ Introductory Problems/IncreasingArray.cpp
@@ -29,3 +29,53 @@ int main()
  
     return 0;
 }
+
+/*
+OR
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+ 
+int main()
+{
+ 
+    long long n;
+ 
+    cin >> n;
+ 
+    vector<long long> vec;
+ 
+    while(n--)
+    {
+        long long n1;
+        cin >> n1;
+        vec.push_back(n1);
+    }
+ 
+    n = vec.size();
+ 
+    long long ans = 0;
+
+    long long maxima = 0;
+ 
+    for(long long i = 0; i < n; i++)
+    {
+        if(vec.at(i) >= maxima)
+        {
+            maxima = max(maxima, vec.at(i));
+        }
+        else
+        {
+            ans += maxima-vec.at(i);
+        }
+    }
+ 
+    cout << ans;
+ 
+ 
+    return 0;
+}
+*/
